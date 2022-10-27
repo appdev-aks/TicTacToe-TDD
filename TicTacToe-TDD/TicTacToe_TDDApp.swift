04ffafbf1a1c2606f27ca6dbf9dae29aa96b62ff
Aks Dev <AksDev@gmail.com>
2022-10-27 22:19:13 +0530

    Initial Commit

---
+++ TicTacToe-TDD/TicTacToe_TDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToe_TDDApp.swift
+//  TicTacToe-TDD
+//
+//  Created by Aks_dev on 27/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToe_TDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
