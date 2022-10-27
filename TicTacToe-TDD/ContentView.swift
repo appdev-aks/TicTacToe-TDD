6cd52c942a26bfc0c549dbba89717d4b4b523546
Aks Dev <AksDev@gmail.com>
2022-10-27 23:04:38 +0530

    Added UI for Boxes

--- TicTacToe-TDD/ContentView.swift
+++ TicTacToe-TDD/ContentView.swift
@@ -11,11 +11,28 @@ struct ContentView: View {
     
     let boxes = [Box(),Box(),Box(),Box(),Box(),Box(),Box(),Box(),Box()]
     
+    let gameColumns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
     var body: some View {
-        VStack {
-           
+        GeometryReader { proxy in
+            VStack {
+                Spacer()
+                    LazyVGrid(columns: gameColumns) {
+                        ForEach(0..<9) { index in
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 5,style: .circular)
+                                    .foregroundColor(.cyan)
+                                .frame(width: proxy.size.width * 0.30,height: proxy.size.width * 0.30,alignment: .bottom)
+                        }
+                    }
+                }
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
